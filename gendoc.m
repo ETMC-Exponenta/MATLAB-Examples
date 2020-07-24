857524a528121e74c6d9d9274890f42b4e9147c5
Pavel Roslovets <roslovets@exponenta.ru>
2020-07-24 16:01:48 +0300

    Добавил рекомендации

--- gendoc.m
+++ gendoc.m
@@ -7,7 +7,13 @@ ds = ds(ds.name ~= ".git", :);
 for sdir = ds.path'
     [~, ds2] = DevUtils.dir(sdir);
     for sdir2 = ds2.path'
-        du.convertMlx(sdir2, 'md', 'Target', fullfile(sdir2, 'README.md'));
+        [fout, fs] = du.convertMlx(sdir2, 'md', 'Target', fullfile(sdir2, 'README.md'), 'Force', 1);
+        if fs.converted(1)
+            txt = DevUtils.readtxt(fout(1), 'Split', true);
+            pdfmsg = sprintf("\n**Рекомендуем смотреть пример в [PDF](%s.pdf)**", fs.shortname(1));
+            txt = [txt(1); pdfmsg; txt(2:end)];
+            DevUtils.writetxt(txt, fout(1));
+        end
         du.convertMlx(sdir2, 'pdf');
         du.convertMlx(sdir2, 'm', 'NamePostfix', '_plain');
     end
